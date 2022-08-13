0a6980e3362e4e0775fee0ad80bd56f57e113448
JohnNaxos <77282784+JohnNaxos@users.noreply.github.com>
2022-08-13 14:04:18 +1000

    from laptop

--- assets/js/scrolling-nav.js
+++ assets/js/scrolling-nav.js
@@ -1,11 +1,12 @@
 $('a[href^="#"]').on('click', function(event) {
 
     var target = $(this.getAttribute('href'));
-  
+
     if (target.length) {
       event.preventDefault();
-      $('html, body').stop().animate({
+    $('html, body').stop().animate({
         scrollTop: target.offset().top - target.height()
       }, 1000);
     }
-  });
+});
+
